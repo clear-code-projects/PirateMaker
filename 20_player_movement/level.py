63380bbd0021ceb15910b5036b15ce618a72b5d4
clear-code-projects <62236986+clear-code-projects@users.noreply.github.com>
2022-10-21 15:06:18 +0100

    Add files via upload

---
+++ 20_player_movement/level.py
@@ -0,0 +1,91 @@
+import pygame, sys 
+from pygame.math import Vector2 as vector
+
+from settings import *
+from support import *
+
+from sprites import Generic, Block, Animated, Particle, Coin, Player, Spikes, Tooth, Shell
+
+class Level:
+	def __init__(self, grid, switch, asset_dict):
+		self.display_surface = pygame.display.get_surface()
+		self.switch = switch
+
+		# groups 
+		self.all_sprites = pygame.sprite.Group()
+		self.coin_sprites = pygame.sprite.Group()
+		self.damage_sprites = pygame.sprite.Group()
+		self.collision_sprites = pygame.sprite.Group()
+
+		self.build_level(grid, asset_dict)
+
+		# additional stuff
+		self.particle_surfs = asset_dict['particle']
+
+	def build_level(self, grid, asset_dict):
+		for layer_name, layer in grid.items():
+			for pos, data in layer.items():
+				if layer_name == 'terrain':
+					Generic(pos, asset_dict['land'][data], [self.all_sprites, self.collision_sprites])
+				if layer_name == 'water':
+					if data == 'top':
+						Animated(asset_dict['water top'], pos, self.all_sprites)
+					else:
+						Generic(pos, asset_dict['water bottom'], self.all_sprites)
+
+				match data:
+					case 0: self.player = Player(pos, self.all_sprites, self.collision_sprites)
+					
+					# coins
+					case 4: Coin('gold', asset_dict['gold'], pos, [self.all_sprites, self.coin_sprites])
+					case 5: Coin('silver', asset_dict['silver'], pos, [self.all_sprites, self.coin_sprites])
+					case 6: Coin('diamond', asset_dict['diamond'], pos, [self.all_sprites, self.coin_sprites])
+
+					# enemies
+					case 7: Spikes(asset_dict['spikes'], pos, [self.all_sprites, self.damage_sprites])
+					case 8: Tooth(asset_dict['tooth'], pos, [self.all_sprites, self.damage_sprites])
+					case 9: Shell('left', asset_dict['shell'], pos, [self.all_sprites, self.collision_sprites])
+					case 10: Shell('right', asset_dict['shell'], pos, [self.all_sprites, self.collision_sprites])
+
+					# palm trees
+					case 11: 
+						Animated(asset_dict['palms']['small_fg'], pos, self.all_sprites)
+						Block(pos, (76,50), self.collision_sprites)
+					case 12: 
+						Animated(asset_dict['palms']['large_fg'], pos, self.all_sprites)
+						Block(pos, (76,50), self.collision_sprites)
+					case 13: 
+						Animated(asset_dict['palms']['left_fg'], pos, self.all_sprites)
+						Block(pos, (76,50), self.collision_sprites)
+					case 14: 
+						Animated(asset_dict['palms']['right_fg'], pos, self.all_sprites)
+						Block(pos + vector(50,0), (76,50), self.collision_sprites)
+					
+					case 15: Animated(asset_dict['palms']['small_bg'], pos, self.all_sprites)
+					case 16: Animated(asset_dict['palms']['large_bg'], pos, self.all_sprites)
+					case 17: Animated(asset_dict['palms']['left_bg'], pos, self.all_sprites)
+					case 18: Animated(asset_dict['palms']['right_bg'], pos, self.all_sprites)
+
+	def get_coins(self):
+		collided_coins = pygame.sprite.spritecollide(self.player, self.coin_sprites, True)
+		for sprite in collided_coins:
+			Particle(self.particle_surfs, sprite.rect.center, self.all_sprites)
+
+	def event_loop(self):
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				sys.exit()
+			if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+				self.switch()
+
+	def run(self, dt):
+		# update
+		self.event_loop()
+		self.all_sprites.update(dt)
+		self.get_coins()
+
+		# drawing
+		self.display_surface.fill(SKY_COLOR)
+		self.all_sprites.draw(self.display_surface)
+		pygame.draw.rect(self.display_surface, 'yellow', self.player.hitbox)
---
+++ 20_player_movement/sprites.py
@@ -0,0 +1,148 @@
+import pygame
+from pygame.math import Vector2 as vector
+
+from settings import *
+
+class Generic(pygame.sprite.Sprite):
+	def __init__(self, pos, surf, group):
+		super().__init__(group)
+		self.image = surf
+		self.rect = self.image.get_rect(topleft = pos)
+
+class Block(Generic):
+	def __init__(self, pos, size, group):
+		surf = pygame.Surface(size)
+		super().__init__(pos, surf, group)
+
+
+# simple animated objects
+class Animated(Generic):
+	def __init__(self, assets, pos, group):
+		self.animation_frames = assets
+		self.frame_index = 0
+		super().__init__(pos, self.animation_frames[self.frame_index], group)
+
+	def animate(self, dt):
+		self.frame_index += ANIMATION_SPEED * dt
+		self.frame_index = 0 if self.frame_index >= len(self.animation_frames) else self.frame_index
+		self.image = self.animation_frames[int(self.frame_index)]
+
+	def update(self, dt):
+		self.animate(dt)
+
+class Particle(Animated):
+	def __init__(self, assets, pos, group):
+		super().__init__(assets, pos, group)
+		self.rect = self.image.get_rect(center = pos)
+
+	def animate(self, dt):
+		self.frame_index += ANIMATION_SPEED * dt
+		if self.frame_index < len(self.animation_frames):
+			self.image = self.animation_frames[int(self.frame_index)]
+		else:
+			self.kill()
+
+class Coin(Animated):
+	def __init__(self, coin_type, assets, pos, group):
+		super().__init__(assets, pos, group)
+		self.rect = self.image.get_rect(center = pos)
+		self.coin_type = coin_type
+
+
+# enemies
+class Spikes(Generic):
+	def __init__(self, surf, pos, group):
+		super().__init__(pos, surf, group)
+
+class Tooth(Generic):
+	def __init__(self, assets, pos, group):
+
+		self.animation_frames = assets
+		self.frame_index = 0
+		self.orientation = 'right'
+		surf = self.animation_frames[f'run_{self.orientation}'][self.frame_index]
+		super().__init__(pos, surf, group)
+		self.rect.bottom = self.rect.top + TILE_SIZE
+
+class Shell(Generic):
+	def __init__(self, orientation, assets, pos, group):
+		self.orientation = orientation
+		self.animation_frames = assets.copy()
+		if orientation == 'right':
+			for key, value in self.animation_frames.items():
+				self.animation_frames[key] = [pygame.transform.flip(surf,True,False) for surf in value]
+
+		self.frame_index = 0
+		self.status = 'idle'
+		super().__init__(pos, self.animation_frames[self.status][self.frame_index], group)
+		self.rect.bottom = self.rect.top + TILE_SIZE
+
+class Player(Generic):
+	def __init__(self, pos, group, collision_sprites):
+		super().__init__(pos, pygame.Surface((80,64)), group)
+		self.image.fill('red')
+
+		# movement
+		self.direction = vector()
+		self.pos = vector(self.rect.center)
+		self.speed = 300
+		self.gravity = 4
+		self.on_floor = False
+
+		# collision
+		self.collision_sprites = collision_sprites
+		self.hitbox = self.rect.inflate(-50,0)
+
+	def input(self):
+		keys = pygame.key.get_pressed()
+		if keys[pygame.K_RIGHT]:
+			self.direction.x = 1
+		elif keys[pygame.K_LEFT]:
+			self.direction.x = -1
+		else:
+			self.direction.x = 0
+
+		if keys[pygame.K_SPACE] and self.on_floor:
+			self.direction.y = -2
+
+	def move(self, dt):
+		
+		# horizontal movement
+		self.pos.x += self.direction.x * self.speed * dt
+		self.hitbox.centerx = round(self.pos.x)
+		self.rect.centerx = self.hitbox.centerx
+		self.collision('horizontal')
+
+		# vertical movement
+		self.pos.y += self.direction.y * self.speed * dt
+		self.hitbox.centery = round(self.pos.y)
+		self.rect.centery = self.hitbox.centery
+		self.collision('vertical')
+
+	def apply_gravity(self, dt):
+		self.direction.y += self.gravity * dt
+		self.rect.y += self.direction.y
+
+	def check_on_floor(self):
+		floor_rect = pygame.Rect(self.hitbox.bottomleft,(self.hitbox.width,2))
+		floor_sprites = [sprite for sprite in self.collision_sprites if sprite.rect.colliderect(floor_rect)]
+		self.on_floor = True if floor_sprites else False
+
+	def collision(self, direction):
+		for sprite in self.collision_sprites:
+			if sprite.rect.colliderect(self.hitbox):
+				if direction == 'horizontal':
+					self.hitbox.right = sprite.rect.left if self.direction.x > 0 else self.hitbox.right
+					self.hitbox.left = sprite.rect.right if self.direction.x < 0 else self.hitbox.left
+					self.rect.centerx, self.pos.x = self.hitbox.centerx, self.hitbox.centerx
+				else: # vertical
+					self.hitbox.top = sprite.rect.bottom if self.direction.y < 0 else self.hitbox.top
+					self.hitbox.bottom = sprite.rect.top if self.direction.y > 0 else self.hitbox.bottom
+					self.rect.centery, self.pos.y = self.hitbox.centery, self.hitbox.centery
+					self.direction.y = 0
+
+	def update(self, dt):
+		self.input()
+		self.apply_gravity(dt)
+		self.move(dt)
+		self.check_on_floor()
