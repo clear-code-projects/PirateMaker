13d08ecf4f25f515cc3e22dba22233c800bd6b00
clear-code-projects <62236986+clear-code-projects@users.noreply.github.com>
2022-10-21 15:08:40 +0100

    Add files via upload

---
+++ 26_level_sky/level.py
@@ -0,0 +1,188 @@
+import pygame, sys 
+from pygame.math import Vector2 as vector
+
+from settings import *
+from support import *
+
+from sprites import Generic, Block, Animated, Particle, Coin, Player, Spikes, Tooth, Shell, Cloud
+
+from random import choice, randint
+
+class Level:
+	def __init__(self, grid, switch, asset_dict):
+		self.display_surface = pygame.display.get_surface()
+		self.switch = switch
+
+		# groups 
+		self.all_sprites = CameraGroup()
+		self.coin_sprites = pygame.sprite.Group()
+		self.damage_sprites = pygame.sprite.Group()
+		self.collision_sprites = pygame.sprite.Group()
+		self.shell_sprites = pygame.sprite.Group()
+
+		self.build_level(grid, asset_dict)
+
+		# level limits
+		self.level_limits = {
+		'left': -WINDOW_WIDTH,
+		'right': sorted(list(grid['terrain'].keys()), key = lambda pos: pos[0])[-1][0] + 500
+		}
+
+		# additional stuff
+		self.particle_surfs = asset_dict['particle']
+		self.cloud_surfs = asset_dict['clouds']
+		self.cloud_timer = pygame.USEREVENT + 2
+		pygame.time.set_timer(self.cloud_timer, 2000)
+		self.startup_clouds()
+
+	def build_level(self, grid, asset_dict):
+		for layer_name, layer in grid.items():
+			for pos, data in layer.items():
+				if layer_name == 'terrain':
+					Generic(pos, asset_dict['land'][data], [self.all_sprites, self.collision_sprites])
+				if layer_name == 'water':
+					if data == 'top':
+						Animated(asset_dict['water top'], pos, self.all_sprites, LEVEL_LAYERS['water'])
+					else:
+						Generic(pos, asset_dict['water bottom'], self.all_sprites, LEVEL_LAYERS['water'])
+
+				match data:
+					case 0: self.player = Player(pos, asset_dict['player'], self.all_sprites, self.collision_sprites)
+					case 1: 
+						self.horizon_y = pos[1]
+						self.all_sprites.horizon_y = pos[1]
+					# coins
+					case 4: Coin('gold', asset_dict['gold'], pos, [self.all_sprites, self.coin_sprites])
+					case 5: Coin('silver', asset_dict['silver'], pos, [self.all_sprites, self.coin_sprites])
+					case 6: Coin('diamond', asset_dict['diamond'], pos, [self.all_sprites, self.coin_sprites])
+
+					# enemies
+					case 7: Spikes(asset_dict['spikes'], pos, [self.all_sprites, self.damage_sprites])
+					case 8: 
+						Tooth(asset_dict['tooth'], pos, [self.all_sprites, self.damage_sprites], self.collision_sprites)
+					case 9: 
+						Shell(
+							orientation = 'left', 
+							assets = asset_dict['shell'], 
+							pos =  pos, 
+							group =  [self.all_sprites, self.collision_sprites, self.shell_sprites],
+							pearl_surf = asset_dict['pearl'],
+							damage_sprites = self.damage_sprites)
+					case 10: 
+						Shell(
+							orientation = 'right', 
+							assets = asset_dict['shell'], 
+							pos =  pos, 
+							group =  [self.all_sprites, self.collision_sprites, self.shell_sprites],
+							pearl_surf = asset_dict['pearl'],
+							damage_sprites = self.damage_sprites)
+
+					# palm trees
+					case 11: 
+						Animated(asset_dict['palms']['small_fg'], pos, self.all_sprites)
+						Block(pos, (76,50), self.collision_sprites)
+					case 12: 
+						Animated(asset_dict['palms']['large_fg'], pos, self.all_sprites)
+						Block(pos, (76,50), self.collision_sprites)
+					case 13: 
+						Animated(asset_dict['palms']['left_fg'], pos, self.all_sprites)
+						Block(pos, (76,50), self.collision_sprites)
+					case 14: 
+						Animated(asset_dict['palms']['right_fg'], pos, self.all_sprites)
+						Block(pos + vector(50,0), (76,50), self.collision_sprites)
+					
+					case 15: Animated(asset_dict['palms']['small_bg'], pos, self.all_sprites, LEVEL_LAYERS['bg'])
+					case 16: Animated(asset_dict['palms']['large_bg'], pos, self.all_sprites, LEVEL_LAYERS['bg'])
+					case 17: Animated(asset_dict['palms']['left_bg'], pos, self.all_sprites, LEVEL_LAYERS['bg'])
+					case 18: Animated(asset_dict['palms']['right_bg'], pos, self.all_sprites, LEVEL_LAYERS['bg'])
+
+		for sprite in self.shell_sprites:
+			sprite.player = self.player
+
+	def get_coins(self):
+		collided_coins = pygame.sprite.spritecollide(self.player, self.coin_sprites, True)
+		for sprite in collided_coins:
+			Particle(self.particle_surfs, sprite.rect.center, self.all_sprites)
+
+	def get_damage(self):
+		collision_sprites = pygame.sprite.spritecollide(self.player, self.damage_sprites, False, pygame.sprite.collide_mask)
+		if collision_sprites:
+			self.player.damage()
+
+	def event_loop(self):
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				sys.exit()
+			if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+				self.switch()
+
+			if event.type == self.cloud_timer:
+				surf = choice(self.cloud_surfs)
+				surf = pygame.transform.scale2x(surf) if randint(0,5) > 3 else surf
+				x = self.level_limits['right'] + randint(100,300)
+				y = self.horizon_y - randint(-50,600)
+				Cloud((x,y), surf, self.all_sprites, self.level_limits['left'])
+
+	def startup_clouds(self):
+		for i in range(40):
+			surf = choice(self.cloud_surfs)
+			surf = pygame.transform.scale2x(surf) if randint(0,5) > 3 else surf
+			x = randint(self.level_limits['left'], self.level_limits['right'])
+			y = self.horizon_y - randint(-50,600)
+			Cloud((x,y), surf, self.all_sprites, self.level_limits['left'])
+
+	def run(self, dt):
+		# update
+		self.event_loop()
+		self.all_sprites.update(dt)
+		self.get_coins()
+		self.get_damage()
+
+		# drawing
+		self.display_surface.fill(SKY_COLOR)
+		self.all_sprites.custom_draw(self.player)
+
+class CameraGroup(pygame.sprite.Group):
+	def __init__(self):
+		super().__init__()
+		self.display_surface = pygame.display.get_surface()
+		self.offset = vector()
+
+	def draw_horizon(self):
+		horizon_pos = self.horizon_y - self.offset.y	
+
+		if horizon_pos < WINDOW_HEIGHT:
+			sea_rect = pygame.Rect(0,horizon_pos,WINDOW_WIDTH,WINDOW_HEIGHT - horizon_pos)
+			pygame.draw.rect(self.display_surface, SEA_COLOR, sea_rect)
+
+			# horizon line 
+			# 3 extra rectangles 
+			horizon_rect1 = pygame.Rect(0,horizon_pos - 10,WINDOW_WIDTH,10)
+			horizon_rect2 = pygame.Rect(0,horizon_pos - 16,WINDOW_WIDTH,4)
+			horizon_rect3 = pygame.Rect(0,horizon_pos - 20,WINDOW_WIDTH,2)
+			pygame.draw.rect(self.display_surface, HORIZON_TOP_COLOR, horizon_rect1)
+			pygame.draw.rect(self.display_surface, HORIZON_TOP_COLOR, horizon_rect2)
+			pygame.draw.rect(self.display_surface, HORIZON_TOP_COLOR, horizon_rect3)
+			pygame.draw.line(self.display_surface, HORIZON_COLOR, (0,horizon_pos), (WINDOW_WIDTH,horizon_pos), 3)
+
+		if horizon_pos < 0:
+			self.display_surface.fill(SEA_COLOR)
+
+	def custom_draw(self, player):
+		self.offset.x = player.rect.centerx - WINDOW_WIDTH / 2
+		self.offset.y = player.rect.centery - WINDOW_HEIGHT / 2
+
+		for sprite in self:
+			if sprite.z == LEVEL_LAYERS['clouds']:
+				offset_rect = sprite.rect.copy()
+				offset_rect.center -= self.offset
+				self.display_surface.blit(sprite.image, offset_rect)
+
+		self.draw_horizon()
+		for sprite in self:
+			for layer in LEVEL_LAYERS.values():
+				if sprite.z == layer and sprite.z != LEVEL_LAYERS['clouds']:
+					offset_rect = sprite.rect.copy()
+					offset_rect.center -= self.offset
+					self.display_surface.blit(sprite.image, offset_rect)
---
+++ 26_level_sky/main.py
@@ -0,0 +1,118 @@
+import pygame
+from pygame.math import Vector2 as vector
+from settings import *
+from support import *
+
+from pygame.image import load
+
+from editor import Editor
+from level import Level
+
+from os import walk
+
+class Main:
+	def __init__(self):
+		pygame.init()
+		self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+		self.clock = pygame.time.Clock()
+		self.imports()
+
+		self.editor_active = True
+		self.transition = Transition(self.toggle)
+		self.editor = Editor(self.land_tiles, self.switch)
+
+		# cursor 
+		surf = load('../graphics/cursors/mouse.png').convert_alpha()
+		cursor = pygame.cursors.Cursor((0,0), surf)
+		pygame.mouse.set_cursor(cursor)
+
+	def imports(self):
+		# terrain
+		self.land_tiles = import_folder_dict('../graphics/terrain/land')
+		self.water_bottom = load('../graphics/terrain/water/water_bottom.png').convert_alpha()
+		self.water_top_animation = import_folder('../graphics/terrain/water/animation')
+
+		# coins
+		self.gold = import_folder('../graphics/items/gold')
+		self.silver = import_folder('../graphics/items/silver')
+		self.diamond = import_folder('../graphics/items/diamond')
+		self.particle = import_folder('../graphics/items/particle')
+
+		# palm trees
+		self.palms = {folder: import_folder(f'../graphics/terrain/palm/{folder}') for folder in list(walk('../graphics/terrain/palm'))[0][1]}
+
+		# enemies
+		self.spikes = load('../graphics/enemies/spikes/spikes.png').convert_alpha()
+		self.tooth = {folder: import_folder(f'../graphics/enemies/tooth/{folder}') for folder in list(walk('../graphics/enemies/tooth'))[0][1]}
+		self.shell = {folder: import_folder(f'../graphics/enemies/shell_left/{folder}') for folder in list(walk('../graphics/enemies/shell_left/'))[0][1]}
+		self.pearl = load('../graphics/enemies/pearl/pearl.png').convert_alpha()
+
+		# player
+		self.player_graphics = {folder: import_folder(f'../graphics/player/{folder}') for folder in list(walk('../graphics/player/'))[0][1]}
+
+		# clouds
+		self.clouds = import_folder('../graphics/clouds')
+
+	def toggle(self):
+		self.editor_active = not self.editor_active
+
+	def switch(self, grid = None):
+		self.transition.active = True
+		if grid:
+			self.level = Level(grid, self.switch,{
+				'land': self.land_tiles,
+				'water bottom': self.water_bottom,
+				'water top': self.water_top_animation,
+				'gold': self.gold,
+				'silver': self.silver,
+				'diamond': self.diamond,
+				'particle': self.particle,
+				'palms': self.palms,
+				'spikes': self.spikes,
+				'tooth': self.tooth,
+				'shell': self.shell,
+				'player': self.player_graphics,
+				'pearl': self.pearl,
+				'clouds': self.clouds
+				})
+
+	def run(self):
+		while True:
+			dt = self.clock.tick() / 1000
+			
+			if self.editor_active:
+				self.editor.run(dt)
+			else:
+				self.level.run(dt)
+			self.transition.display(dt)
+			pygame.display.update()
+
+
+class Transition:
+	def __init__(self, toggle):
+		self.display_surface = pygame.display.get_surface()
+		self.toggle = toggle
+		self.active = False
+
+		self.border_width = 0
+		self.direction = 1
+		self.center = (WINDOW_WIDTH /2, WINDOW_HEIGHT / 2)
+		self.radius = vector(self.center).magnitude()
+		self.threshold = self.radius + 100
+
+	def display(self, dt):
+		if self.active:
+			self.border_width += 1000 * dt * self.direction
+			if self.border_width >= self.threshold:
+				self.direction = -1
+				self.toggle()
+			
+			if self.border_width < 0:
+				self.active = False
+				self.border_width = 0
+				self.direction = 1
+			pygame.draw.circle(self.display_surface, 'black',self.center, self.radius, int(self.border_width))
+
+if __name__ == '__main__':
+	main = Main()
+	main.run() 
